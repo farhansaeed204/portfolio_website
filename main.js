ceec3e17c80e2e085bd7fb8579a3bdf1973477ba
Farhan_Saeed <128322226+farhansaeed204@users.noreply.github.com>
2025-11-13 12:38:01 +0500

    Add auto typing functionality and clean up code

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // navbar toggle functionality
 let menubar = document.getElementById("menubar");
 let navlist = document.getElementById("navlist");
@@ -5,6 +6,15 @@ let navlist = document.getElementById("navlist");
 menubar.addEventListener("click", ()=>{
     navlist.classList.toggle("show");
 });
+
+// add auto type functioanlity
+let word = document.getElementById("auto-type")
+let typed = new Typed("#auto-type",{
+    strings: ["M.FARHAN", "A PASSIONATE DEVELOPER"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
 
 
 // form validation functionality
